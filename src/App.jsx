b5c23d9f5ed2b762bea2717082ba3898bc6c4452
obuhdaniel <danielobuh05@gmail.com>
2024-11-04 14:07:58 +0100

    for the new repo

--- src/App.jsx
+++ src/App.jsx
@@ -22,26 +22,26 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/ACES-MHS" element={<Homepage />} />
-        <Route path="/ACES-MHS/adhd" element={<ADHD />} />
-        <Route path="/ACES-MHS/anxiety" element={<Anxiety />} />
-        <Route path="/ACES-MHS/bipolar" element={<Bipolar />} />
-        <Route path="/ACES-MHS/depression" element={<Depression />} />
-        <Route path="/ACES-MHS/trivia" element={<TriviaPage />} />
-        <Route path="/ACES-MHS/ptsd" element={<PTSD />} />
-        <Route path="/ACES-MHS/adhd-result" element={<MhtResult />} />
-        <Route path="/ACES-MHS/anxiety-result" element={<MhtResuAnxiety />} />
-        <Route path="/ACES-MHS/bipolar-result" element={<MhtResuBipolar />} />
-        <Route path="/ACES-MHS/test" element={<TakeTest/>} />
-        <Route path="/ACES-MHS/quiz" element={<TakeQuiz/>} />
-        <Route path="/ACES-MHS/more/mhs" element={<MHS />} />
+        <Route path="/mhs" element={<Homepage />} />
+        <Route path="/mhs/adhd" element={<ADHD />} />
+        <Route path="/mhs/anxiety" element={<Anxiety />} />
+        <Route path="/mhs/bipolar" element={<Bipolar />} />
+        <Route path="/mhs/depression" element={<Depression />} />
+        <Route path="/mhs/trivia" element={<TriviaPage />} />
+        <Route path="/mhs/ptsd" element={<PTSD />} />
+        <Route path="/mhs/adhd-result" element={<MhtResult />} />
+        <Route path="/mhs/anxiety-result" element={<MhtResuAnxiety />} />
+        <Route path="/mhs/bipolar-result" element={<MhtResuBipolar />} />
+        <Route path="/mhs/test" element={<TakeTest/>} />
+        <Route path="/mhs/quiz" element={<TakeQuiz/>} />
+        <Route path="/mhs/more/mhs" element={<MHS />} />
         <Route
-          path="/ACES-MHS/depression-result"
+          path="/mhs/depression-result"
           element={<MhtResuDepression />}
         />
-        <Route path="/ACES-MHS/trivia" element={<TriviaPage />} />
-        <Route path="/ACES-MHS/trivia-result" element={<Results />} />
-        <Route path="/ACES-MHS/ptsd-result" element={<MhtResuPTSD />} />
+        <Route path="/mhs/trivia" element={<TriviaPage />} />
+        <Route path="/mhs/trivia-result" element={<Results />} />
+        <Route path="/mhs/ptsd-result" element={<MhtResuPTSD />} />
 
   
         <Route path="*" element={<PageNotFound />} />
