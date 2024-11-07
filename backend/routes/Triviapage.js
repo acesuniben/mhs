76404061e08a901bc28faf27833980ce56af319e
obuhdaniel <danielobuh05@gmail.com>
2024-11-07 08:32:36 +0000

    Nov 7, 2024, 9:32 AM

--- backend/routes/Triviapage.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const User = require('../src/models/User')
-
-router.post('/', async (req, res) => {
-    const {data} = req.body
-//use user id to access database and record user result
-
-})
